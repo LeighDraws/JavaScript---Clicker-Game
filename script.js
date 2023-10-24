fe16b6aec5df4d36dca7f49da6c760ab350459f9
Leigh <145121575+LeighDraws@users.noreply.github.com>
2023-10-24 19:43:33 +0200

    script.js
    Updated! 
    Les boutons fonctionnent tous et le clic gauche ne prévaut pas sur le double clic

--- script.js
+++ script.js
@@ -1,30 +1,73 @@
-const allBlueButtons = document.querySelectorAll(".blue");
-// Liste tous les boutons ayant la classe "blue", chaque bouton est un élement du tableau allBlueButtons (?)
-
-function blueButtonClicked() {
-    this.classList.add("clicked");
-    console.log("blue button clicked!") 
-}
-// Cette fonction ajoute la classe "clicked" aux boutons bleus cliqués avec le clic gauche (grâce à "this"), la classe cache les boutons 
-
-function handleRightClick(event) {
+// Cette fonction empêche le menu déroulant de s'ouvrir quand on fait un clic droit sur les boutons bleus et jaunes (et affiche un message pour l'utilisateur)
+function preventRightClick(event) {
     event.preventDefault();
-    console.log("Clic droit :(") 
+    document.getElementById("message").textContent = "Clic droit :( ! Réessayez"
 }
-// Cette fonction empêche le menu déroulant de s'ouvrir quand on fait un clic droit sur le bouton bleu (et affiche "clic droit" dans la console)
-  
-function handleDoubleClick(event) {
-    console.log("Double clic!");
+
+// Cette fonction va servir à activer le bouton rouge, en le faisant disparaitre avec la classe 'clicked' et en affichant un message pour l'utilisateur
+function redButtonClicked(event) {
+    event.preventDefault();
+    this.classList.add("clicked");
+    document.getElementById("message").textContent = "Bouton rouge cliqué !"
 }
-// Cette fonction affiche "double clic" dans la console
-// Pour le moment, le clic gauche empêche la possibilité de faire un double clic
 
+// Initialise un timer pour empêcher l'utilisation du clic gauche quand l'utilisateur double clique
+let timer
 
+// Liste tous les boutons ayant la classe "blue"
+const allBlueButtons = document.querySelectorAll(".blue");
+
+// Pour chaque boutons bleus, cette fonction écoute l'évèment 'click' (gauche) et démarre le timer, si un clic supplémentaire a été entendu après 200ms c'est un double clic, si non c'est un clic gauche normal. La dernière ligne appelle la fonction "preventRightClick" qui empèche le menu déroulant de s'ouvrir.
 allBlueButtons.forEach(button => {
-button.addEventListener("click", blueButtonClicked);
-button.addEventListener("contextmenu", handleRightClick);
-button.addEventListener("dblclick", handleDoubleClick)
-});
-// Cette méthode execute les fonctions données : blueButtonClicked, handleRightClick et handleDoubleClick; pour chaque élément du tableau "allBlueButtons" 
+    button.addEventListener("click", event => {
+        if (event.detail === 1) {
+            timer = setTimeout(() => {
+                event.target.classList.add("clicked");
+                document.getElementById("message").textContent = "Bouton bleu cliqué !"
+            }, 200)
+        }
+    })
+    button.addEventListener("dblclick", event => {
+        clearTimeout(timer);
+        document.getElementById("message").textContent = "Double clic :( ! Réessayez"
+    })
+    button.addEventListener("contextmenu", preventRightClick)
+})
 
-  
+// Liste tous les boutons ayant la classe "red"
+const allRedButtons = document.querySelectorAll(".red");
+
+
+allRedButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        if (event.detail === 1) {
+            timer = setTimeout(() => {
+                document.getElementById("message").textContent = "Clic gauche :( ! Réessayez"
+            }, 200)
+        }
+    })
+    button.addEventListener("dblclick", event => {
+        clearTimeout(timer);
+        document.getElementById("message").textContent = "Double clic :( ! Réessayez"
+    })
+    button.addEventListener("contextmenu", redButtonClicked)
+})
+
+// Liste tous les boutons ayant la classe "yellow"
+const allYellowButtons = document.querySelectorAll(".yellow");
+
+allYellowButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        if (event.detail === 1) {
+            timer = setTimeout(() => {
+                document.getElementById("message").textContent = "Clic gauche :( ! Réessayez"
+            }, 200)
+        }
+    })
+    button.addEventListener("dblclick", event => {
+        clearTimeout(timer);
+        event.target.classList.add("clicked");
+        document.getElementById("message").textContent = "Bouton jaune cliqué !"
+    })
+    button.addEventListener("contextmenu", preventRightClick)
+})
